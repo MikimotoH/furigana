a6786fa83859ba4ec28de19668521c370fe95624
MikimotoH <MikimotoH@gmail.com>
2017-07-28 15:01:35 +0800

    set version to 0.1

--- setup.py
+++ setup.py
@@ -1,37 +1,36 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import setuptools
 
-setup(name='furigana', version='1.0',
-        description='''convert Kanji in Japanese into Kanji superscript with Furigana(振り仮名).  For example, "澱んだ街角" => "澱(よど)んだ街角(まちかど)" ''',
-        author='Miki.Liu',
-        url='https://github.com/MikimotoH/furigana',
-        packages=['furigana'],
-        install_requires=['mecab-python3']
-        python_requires='>=3',
-        classifiers=[
-            # How mature is this project? Common values are
-            #   3 - Alpha
-            #   4 - Beta
-            #   5 - Production/Stable
-            'Development Status :: 3 - Alpha',
+setup(name='furigana', 
+	version='0.1',
+	description='''convert Kanji in Japanese into Kanji with Hiragana placed above (Furigana(振り仮名)). For example, "澱んだ街角" => "澱(よど)んだ街角(まちかど)" ''',
+	author='Miki.Liu',
+	url='https://github.com/MikimotoH/furigana',
+	packages=['furigana'],
+	install_requires=['mecab-python3']
+	python_requires='>=3',
+	classifiers=[
+	    # How mature is this project? Common values are
+	    #   3 - Alpha
+	    #   4 - Beta
+	    #   5 - Production/Stable
+	    'Development Status :: 3 - Alpha',
 
-            # Indicate who your project is intended for
-            'Intended Audience :: Developers',
-            'Topic :: Software Development :: Build Tools',
+	    # Indicate who your project is intended for
+	    'Intended Audience :: Developers',
+	    'Topic :: Software Development :: Build Tools',
 
-            # Pick your license as you wish (should match "license" above)
-            'License :: OSI Approved :: MIT License',
+	    # Pick your license as you wish (should match "license" above)
+	    'License :: OSI Approved :: MIT License',
 
-            # Specify the Python versions you support here. In particular, ensure
-            # that you indicate whether you support Python 2, Python 3 or both.
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.2',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4',
-            ],
-        keywords='Japanese Language Processing',
-        packages=['furigana']
-        )
-
+	    # Specify the Python versions you support here. In particular, ensure
+	    # that you indicate whether you support Python 2, Python 3 or both.
+	    'Programming Language :: Python :: 3',
+	    'Programming Language :: Python :: 3.2',
+	    'Programming Language :: Python :: 3.3',
+	    'Programming Language :: Python :: 3.4',
+	    ],
+	keywords='Japanese Language Processing',
+	packages=['furigana']
+	)
